ee915f4c980bfeb3bfc299d4b084ea2a48f73eaa
Rafał Gałek <riper384@gmail.com>
2018-11-24 19:54:47 +0100

    navbar

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,19 +3,30 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Redirect } from 'react-router';
 
 import Navbar from './container/navbar/Navbar';
+import Home from './container/home/Home';
 
 export default class Routes extends Component {
   constructor() {
     super();
     this.state = {
-      isLogged: true
+      isLogged: true,
+      whatPath: null
     };
+  }
+
+  setPath = (arg) => {
+    this.setState({
+      Path: arg
+    });
   }
 
   render() {
     return (
       <BrowserRouter>
-        <Navbar isLogged={this.state.isLogged} />
+        <Navbar isLogged={this.state.isLogged} Path={this.state.whatPath} setPath={this.setPath} />
+        {/* <Switch>
+          <Route path="/" component={Home} exact />
+        </Switch> */}
       </BrowserRouter>
     );
   }
