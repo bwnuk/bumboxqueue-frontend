6d0783f70373e8ad9fefbe4a5e92b9027bc75d2d
Rafał Gałek <riper384@gmail.com>
2018-11-24 19:54:47 +0100

    navbar

--- src/container/navbar/Navbar.jsx
+++ src/container/navbar/Navbar.jsx
@@ -3,11 +3,19 @@ import { NavLink } from 'react-router-dom';
 
 import './Navbar.scss';
 
+const NavItem = (props) => {
+  const pageURI = window.location.pathname + window.location.search;
+  const liClassName = (props.path === pageURI) ? 'nav-item active' : 'nav-item';
+  return (
+    <li className={liClassName}><NavLink className="nav-link" exact to={props.href}>{props.name}</NavLink></li>
+  );
+};
+
 export default class Navigation extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <nav className="navbar navbar-expand-sm navbar-dark fixed-top bg-dark">
           <NavLink className="navbar-brand" exact to="/">Bumbox Queue</NavLink>
           <button
             className="navbar-toggler"
@@ -22,19 +30,13 @@ export default class Navigation extends Component {
           </button>
           <div className="collapse navbar-collapse" id="navbarCollapse">
             <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <NavLink className="nav-link" exact to="./home">Home</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" exact to="./profile">Profile</NavLink>
-              </li>
-              <li>
-                <NavLink className="nav-link" exact to="./login">Log in</NavLink>
-              </li>
-              <li>
-                <NavLink className="nav-link" exact to="./register">Sing up</NavLink>
-              </li>
+              <NavItem href="/home" name="Home" />
+              <NavItem href="/profile" name="Profile" />
             </ul>
+            <div className="form-inline mt-3 mt-sm-0 row d-flex justify-content-center">
+              <NavLink className="nav-link btn btn btn-outline-warning m-1" exact to="./login">Log in</NavLink>
+              <NavLink className="nav-link btn btn btn-outline-warning m-1" exact to="./register">Sing up</NavLink>
+            </div>
           </div>
         </nav>
       </div>
