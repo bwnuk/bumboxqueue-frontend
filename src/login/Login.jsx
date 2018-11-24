f88cecc41088c33fe43920dda68c4a2a33258dff
Rafał Gałek <riper384@gmail.com>
2018-11-24 21:46:53 +0100

    added login for doctor and patient

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
 import './Login.scss';
+import LoginPatient from './LoginPatient';
+import LoginDoctor from './LoginDoctor';
 
 export default class Login extends Component {
   render() {
     return (
-      <div className="row" />
+      <div className="container">
+        <div className="text-center row">
+          <LoginPatient />
+          <LoginDoctor />
+        </div>
+      </div>
     );
   }
 }
---
+++ src/profile/Profile.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row text-center">
+          <div className="col">
+            <h1>Profile</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
