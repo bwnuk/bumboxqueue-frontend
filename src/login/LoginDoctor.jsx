54133aebafd47df6e2e6fc166fc85e0fb511a404
Rafał Gałek <riper384@gmail.com>
2018-11-24 19:55:03 +0100

    init login

---
+++ src/login/LoginDoctor.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './Login.scss';
+
+export default class LoginDoctor extends Component {
+  render() {
+    return (
+      <div>
+        Home
+      </div>
+    );
+  }
+}
